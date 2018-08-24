cd4cbd67b9138be76df9e24bee3d81904329bf1e
Sheppyi Desktop <hirschmann.tim@t-online.de>
2018-08-24 20:27:10 +0200

    start

---
+++ brumbrum2/Assets/Scripts/Player/MovementController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(PhysicsController))]
+public class MovementController : MonoBehaviour {
+
+    PhysicsController psC;
+    Vector3 direction = new Vector3(0 , 0 , 0);
+    Vector3 worldDirection = new Vector3(0, 0, 0);
+    float rotation = 30;
+    float facing = 0;
+    float tilt = 10;
+
+    Vector3 gravityDirection = new Vector3(0,-1,0);     //angle from car
+    float gravityStrength = 80;
+    Vector3 maxGravitySpeed = new Vector3(100,100,100);
+    
+
+
+
+    //bools
+    bool gravityEnabled = true;
+
+    private void Start() {
+        psC = this.GetComponent<PhysicsController>();
+    }
+
+
+    private void Update() {
+        ApplyGravity();
+        
+        
+        psC.Move(direction * Time.deltaTime ,rotation, facing, tilt, worldDirection * Time.deltaTime);
+    }
+
+
+
+    void ApplyGravity() {
+        if (gravityEnabled) {
+            worldDirection += gravityDirection * gravityStrength * Time.deltaTime;
+            if (Mathf.Abs(worldDirection.x) > maxGravitySpeed.x ) {
+                Mathf.Clamp(worldDirection.x, -maxGravitySpeed.x, maxGravitySpeed.x);
+            }
+            if (Mathf.Abs(worldDirection.y) > maxGravitySpeed.y) {
+                Mathf.Clamp(worldDirection.y, -maxGravitySpeed.y, maxGravitySpeed.y);
+            }
+            if (Mathf.Abs(worldDirection.z) > maxGravitySpeed.z) {
+                Mathf.Clamp(worldDirection.z, -maxGravitySpeed.z, maxGravitySpeed.z);
+            }
+        }
+    }
+
+
+
+
+
+    public void ChangeGravity(Vector3 newGravityDirection , float newGravityStrength) {
+        gravityDirection = newGravityDirection;
+        gravityStrength = newGravityStrength;
+    }
+}
---
+++ brumbrum2/Assets/Scripts/Player/PhysicsController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PhysicsController : MonoBehaviour {
+
+
+    //references
+    RaycastOrigins ro;
+    public LayerMask collisionMask;
+
+
+    //gamevariables
+    Vector3 finalDirection;
+    
+
+
+    //internal
+    Vector3 extents;    //extends of collider
+
+
+
+    private void Start() {
+        extents = this.GetComponent<Collider>().bounds.extents;
+    }
+
+
+
+
+
+    public void Move(Vector3 direction, float rotation,float facing, float tilt,Vector3 worldDirecton) {
+        finalDirection += -finalDirection + -transform.right * direction.x + transform.forward * direction.y + transform.up * direction.z + worldDirecton;
+
+        CheckCollisions();
+
+        
+
+
+
+
+
+        transform.localRotation = Quaternion.Euler(0, transform.localRotation.y, transform.localRotation.z);
+        transform.position += finalDirection;
+        transform.localRotation = Quaternion.Euler(rotation, facing, -tilt);
+    }
+
+
+
+    void CheckCollisions() {
+        UpdateRayCastOrigins();
+
+        RaycastHit hit;
+        if (finalDirection.y <= 0 && Physics.Raycast(ro.bottomLeftFront, Vector3.down, out hit, Mathf.Abs(finalDirection.y), collisionMask)) {
+            finalDirection.y = finalDirection.y > 0 ? 1 : -1;
+            if (Mathf.Abs(hit.distance) <= 0.01f) {
+                finalDirection.y = 0;
+            }
+            else {
+                finalDirection.y *= (hit.distance - 0.01f);
+            }
+        }
+        else if (finalDirection.y >= 0 && Physics.Raycast(ro.bottomLeftFront, Vector3.up, out hit, Mathf.Abs(finalDirection.y), collisionMask)) {
+            finalDirection.y = finalDirection.y > 0 ? 1 : -1;
+            if (Mathf.Abs(hit.distance) <= 0.01f) {
+                finalDirection.y = 0;
+            }
+            else {
+                finalDirection.y *= (hit.distance - 0.01f);
+            }
+        }
+    }
+
+
+    void UpdateRayCastOrigins() {
+        var thisMatrix = this.transform.localToWorldMatrix;
+        ro.topRightBack = thisMatrix.MultiplyPoint3x4(extents);
+        ro.topRightFront = thisMatrix.MultiplyPoint3x4(new Vector3(-extents.x, extents.y, extents.z));
+        ro.topLeftBack = thisMatrix.MultiplyPoint3x4(new Vector3(extents.x, extents.y, -extents.z));
+        ro.topLeftFront = thisMatrix.MultiplyPoint3x4(new Vector3(-extents.x, extents.y, -extents.z));
+        ro.bottomRightBack = thisMatrix.MultiplyPoint3x4(new Vector3(extents.x, -extents.y, extents.z));
+        ro.bottomRightFront= thisMatrix.MultiplyPoint3x4(new Vector3(-extents.x, -extents.y, extents.z));
+        ro.bottomLeftBack = thisMatrix.MultiplyPoint3x4(new Vector3(extents.x, -extents.y, -extents.z));
+        ro.bottomLeftFront = thisMatrix.MultiplyPoint3x4(-extents);
+    }
+
+
+
+    struct RaycastOrigins {
+        public Vector3 topLeftFront, topRightFront, bottomLeftFront, bottomRightFront;
+        public Vector3 topLeftBack, topRightBack, bottomLeftBack, bottomRightBack;
+    }
+}
